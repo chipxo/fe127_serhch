68c4f06cf4c8c0fa689d85813993d109c049fc1d
Serhii Chyipesh <chiypesh200059@gmail.com>
2023-11-21 18:08:15 +0200

    ex1

--- ex1/script.js
+++ ex1/script.js
@@ -1,6 +1,10 @@
 let burger = document.querySelector(".burger");
 let header = document.querySelector(".header");
+let main = document.querySelector(".main");
+let footer = document.querySelector(".footer");
 burger.addEventListener("click", function () {
   burger.classList.toggle("active");
   header.classList.toggle("active");
+  main.classList.toggle("blur");
+  footer.classList.toggle("blur");
 });
