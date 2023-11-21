8400d9c09314f782297bf3093796d86f0c9f20c8
Serhii Chyipesh <chiypesh200059@gmail.com>
2023-11-22 01:51:14 +0200

    ex1

--- ex1/script.js
+++ ex1/script.js
@@ -1,10 +1,28 @@
-let burger = document.querySelector(".burger");
-let header = document.querySelector(".header");
-let main = document.querySelector(".main");
-let footer = document.querySelector(".footer");
+const body = document.querySelector(".body");
+const burger = document.querySelector(".burger");
+const header = document.querySelector(".header");
+const main = document.querySelector(".main");
+const footer = document.querySelector(".footer");
+const anchors = document.querySelectorAll(".anchor");
 burger.addEventListener("click", function () {
   burger.classList.toggle("active");
   header.classList.toggle("active");
   main.classList.toggle("blur");
   footer.classList.toggle("blur");
 });
+for (const anchor of anchors) {
+  anchor.addEventListener("click", function () {
+    anchor.classList.toggle("hide");
+    body.classList.toggle("hidden");
+    header.classList.remove("active");
+    burger.classList.remove("active");
+    main.classList.remove("blur");
+    footer.classList.remove("blur");
+  });
+}
+setInterval(() => {
+  body.classList.remove("hidden");
+  for (const anchor of anchors) {
+    anchor.classList.remove("hide");
+  }
+}, 1000);
