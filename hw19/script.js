1dd4e98ed584892546822c6014ff82c15883465b
Serhii Chyipesh <chiypesh200059@gmail.com>
2023-12-15 02:49:42 +0200

    hw19

--- hw19/script.js
+++ hw19/script.js
@@ -35,25 +35,53 @@ headerElement.appendChild(logoLink);
 
 let iconElement = document.createElement("i");
 iconElement.className = header.logo.iconClass;
-headerElement.appendChild(iconElement); //
+headerElement.appendChild(iconElement);
 
 let nav = document.createElement("nav");
+
+let ul = document.createElement("ul");
+
 for (let key in header.nav) {
+  let navItem = document.createElement("li");
   let navLink = document.createElement("a");
   navLink.href = header.nav[key].url;
   navLink.textContent = header.nav[key].text;
-  navLink.classList.add("nav-link");
-  nav.appendChild(navLink);
+  navItem.appendChild(navLink);
+  ul.appendChild(navItem);
 }
+
+nav.appendChild(ul);
 headerElement.appendChild(nav);
 
 let btnContainer = document.createElement("div");
+btnContainer.classList.add("header-btn");
+iconElement.addEventListener("click", () => {
+  btnContainer.classList.toggle("header-btn");
+  btnContainer.classList.toggle("nav-btn");
+});
 header.btn.forEach((btnText) => {
   let btn = document.createElement("button");
   btn.textContent = btnText;
-  btn.classList.add("header-btn");
+  btn.classList.add("btn");
   btnContainer.appendChild(btn);
 });
+
+iconElement.addEventListener("click", () => {
+  ul.classList.toggle("nav-link");
+});
+
+const removeClassIfLargeScreen = () => {
+  if (window.innerWidth > 1024) {
+    ul.classList.remove("nav-link");
+    btnContainer.classList.remove("nav-btn");
+    btnContainer.classList.add("header-btn");
+  }
+};
+
+removeClassIfLargeScreen();
+
+window.addEventListener("resize", removeClassIfLargeScreen);
+
 headerElement.appendChild(btnContainer);
 
 document.body.appendChild(headerElement);
