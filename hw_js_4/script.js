0a44c899516e9cacfc1308c120998d5389f2db07
Serhii Chyipesh <chiypesh200059@gmail.com>
2023-12-18 20:10:59 +0200

    hw 4 js

---
+++ hw_js_4/script.js
@@ -0,0 +1,36 @@
+let red = document.querySelector("#red");
+let yellow = document.querySelector("#yellow");
+let green = document.querySelector("#green");
+let btn = document.querySelector("#switchLight");
+let clickCount = 0;
+
+const colors = [red, yellow, green];
+const colorsName = ["red", "yellow", "green"];
+
+const changeColor = () => {
+  let currentColor = colors[clickCount];
+
+  let currentColorName = colorsName[clickCount];
+
+  const filterColors = colors.filter((color) => color !== currentColor);
+
+  const filterColorsName = colorsName.filter(
+    (color) => color !== currentColorName,
+  );
+
+  currentColor.classList.remove(`${colorsName[clickCount]}-dark`);
+  currentColor.classList.add(`${colorsName[clickCount]}-bright`);
+  filterColors[0].classList.add(`${filterColorsName[0]}-dark`);
+  filterColors[1].classList.add(`${filterColorsName[1]}-dark`);
+
+  clickCount <= 1 ? clickCount++ : (clickCount = 0);
+};
+changeColor();
+
+btn.addEventListener("click", changeColor);
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    changeColor();
+  }
+});
